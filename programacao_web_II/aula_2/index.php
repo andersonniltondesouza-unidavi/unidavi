ccf7b76bfe08984bb99b51256cd30f2fa0d74169
andersonniltondesouza-unidavi <anderson.souza@unidavi.edu.br>
2025-02-22 12:52:40 -0300

    Add files via upload

---
+++ programacao_web_II/aula_2/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PW2 - Atividade 01 - 20.02 </title>
+</head>
+<body>
+    <form action="index.php" method="post">
+    <label for="nome">Nome</label>
+    <input type="text" id="nome" class="input" required><br>
+    <label for="telefone">Telefone</label>
+    <input type="numeric" id="telefone" class="input" required><br>
+    <label for="email">E-mail</label>
+    <input type="text" id="email" class="input" required><br>
+    <label for="mensagem">Mensagem</label>
+    <input type="text" id="mensagem" class="input" required><br>
+    </form>
+</body>
+</html>
