876f1c610676f1eeebbab051ee0e8eb20b802a7a
andersonniltondesouza-unidavi <anderson.souza@unidavi.edu.br>
2025-02-26 19:20:29 -0300

    Delete DesenvWeb2024-main directory
    commit

--- DesenvWeb2024-main/DesenvWeb2024-main/hospital/js/cadastrar_setor.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const voltarButton = document.querySelector('.voltar-button');
-    voltarButton.addEventListener('click', function() {
-        window.location.href = 'admin_dashboard.html';
-    });
-});
