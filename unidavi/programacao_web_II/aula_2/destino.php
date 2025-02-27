ea1d463c5c3df6976165cbd49c104cd4b650b5e3
andersonniltondesouza.unidavi <anderson.souza@unidavi.edu.br>
2025-02-27 19:00:52 -0300

    commit

--- unidavi/programacao_web_II/aula_2/destino.php
+++ unidavi/programacao_web_II/aula_2/destino.php
@@ -1,9 +1,9 @@
 <?php
     echo '<h1>Dados Recebidos</h1>';
-    echo 'Nome: ' . htmlspecialchars($_REQUEST['nome']) . ' <br>';
-    echo 'Telefone: ' . htmlspecialchars($_REQUEST['telefone']) . '<br>';
-    echo 'E-mail: ' . htmlspecialchars($_REQUEST['email']) . '<br>';
-    echo 'Mensagem: ' . nl2br(htmlspecialchars($_REQUEST['mensagem'])) . '<br><br>';
+    echo 'Nome: ' . ($_REQUEST['nome']) . ' <br>';
+    echo 'Telefone: ' . ($_REQUEST['telefone']) . '<br>';
+    echo 'E-mail: ' . ($_REQUEST['email']) . '<br>';
+    echo 'Mensagem: ' . nl2br(($_REQUEST['mensagem'])) . '<br><br>';
 
     echo '<h2>Método HTTP utilizado:</h2>';
     echo $_SERVER['REQUEST_METHOD'] . '<br><br>';
@@ -11,5 +11,5 @@
     echo '<h2>Cabeçalhos da Requisição:</h2>';
     $headers = apache_request_headers();
     foreach ($headers as $header => $value) 
-        echo htmlspecialchars($header) . ': ' . htmlspecialchars($value) . '<br>';
+        echo ($header) . ': ' . ($value) . '<br>';
 ?>
