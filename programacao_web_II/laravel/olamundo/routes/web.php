a6d806ac858b397bd6aa95820e4781e5dbb7ae16
andersonniltondesouza.unidavi <anderson.souza@unidavi.edu.br>
2025-03-20 19:53:21 -0300

    feat: olamundo

---
+++ programacao_web_II/laravel/olamundo/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\SobreController;
+use Illuminate\Support\Facades\View;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::resource('/contato', ContatoController::class);
+
+Route::get('/sobre',[SobreController::class,'sobre']);
