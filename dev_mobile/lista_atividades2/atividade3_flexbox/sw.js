325329595f88893ccbe80d58ed877694fee24079
andersonniltondesouza <anderson.souza@unidavi.edu.br>
2025-09-10 22:45:26 -0300

    commit 10.09

---
+++ dev_mobile/lista_atividades2/atividade3_flexbox/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "meu-pwa-cache-v1";
+const URLS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",    
+  "/img/banner-unidavi (1).png",
+  "/img/unidavi-logo (1).png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
