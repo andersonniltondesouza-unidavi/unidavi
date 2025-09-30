c930931b79fcc1bbeae5889fb8915a62f3b2b008
andersonniltondesouza <anderson.souza@unidavi.edu.br>
2025-09-30 19:07:04 -0300

    commit

---
+++ dev_mobile/flutter/atv_sample/android/app/src/main/kotlin/com/example/atv_sample/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.atv_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ dev_mobile/flutter/atv_sample/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const SpacedItemsList());
+
+class SpacedItemsList extends StatelessWidget {
+  const SpacedItemsList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const items = 6;
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        cardTheme: CardThemeData(color: Colors.yellowAccent),
+      ),
+      home: Scaffold(
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: List.generate(
+                    items,
+                    (index) => ItemWidget(text:'Cartão ${index + 1}'),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class ItemWidget extends StatelessWidget {
+  const ItemWidget({super.key, required this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: SizedBox(height: 100, child: Row(mainAxisAlignment: MainAxisAlignment.center ,children: [Icon(Icons.access_alarms_outlined, color: Colors.lightGreen), Text(text, style:TextStyle(color: Colors.red),), Icon(Icons.star, color: Colors.lightGreen) ] )),
+    );
+  }
+}
